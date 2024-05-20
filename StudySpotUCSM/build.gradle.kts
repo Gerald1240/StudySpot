128947620525b7a2607d0fe000c13688f58d54ef
Gerald1240 <111760826+Gerald1240@users.noreply.github.com>
2024-05-20 15:59:54 -0500

    Add files via upload

--- StudySpotUCSM/build.gradle.kts
+++ StudySpotUCSM/build.gradle.kts
@@ -1,9 +1,14 @@
 buildscript {
     dependencies {
         classpath(libs.google.services)
-        classpath ("com.google.gms:google-services:4.4.1")
+    }
+    repositories {
+        google()
+        mavenCentral()
+        maven{url = uri ("https://jitpack.io")}
     }
 }
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
--- StudySpotUCSM/settings.gradle.kts
+++ StudySpotUCSM/settings.gradle.kts
@@ -9,16 +9,23 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-android/maven/v1") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-android/maven/v1") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url =uri("https://pkgs.dev.azure.com/MicrosoftDeviceSDK/DuoSDK-Public/_packaging/Duo-SDK-Feed/maven/v1") }
     }
 }
 
 rootProject.name = "StudySpot UCSM"
 include(":app")
- 
